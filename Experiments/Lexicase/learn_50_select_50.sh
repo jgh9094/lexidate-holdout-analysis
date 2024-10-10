acdca4821ec5d6fa144321c57cf47f2ac90cb1d0
Jose <jgh9094@gmail.com>
2024-10-10 15:54:42 -0700

    more memory (200GB)

--- Experiments/Lexicase/learn_50_select_50.sh
+++ Experiments/Lexicase/learn_50_select_50.sh
@@ -5,7 +5,7 @@
 #SBATCH --array=1-480%16
 #SBATCH --cpus-per-task=9
 #SBATCH -t 72:00:00
-#SBATCH --mem=150GB
+#SBATCH --mem=200GB
 #SBATCH --job-name=l-5-5
 #SBATCH -p defq,moore
 #SBATCH --exclude=esplhpc-cp040
