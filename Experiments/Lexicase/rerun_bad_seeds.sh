61f11f2210cbb9a777c14bdd090aeed9dd825d6a
Jose <jgh9094@gmail.com>
2024-11-13 11:51:14 -0800

    rerun seeds

--- Experiments/Lexicase/rerun_bad_seeds.sh
+++ Experiments/Lexicase/rerun_bad_seeds.sh
@@ -2,8 +2,8 @@
 ########## Define Resources Needed with SBATCH Lines ##########
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --array=1,2
-#SBATCH --cpus-per-task=12
+#SBATCH --array=1,2,3,4
+#SBATCH --cpus-per-task=10
 #SBATCH -t 72:00:00
 #SBATCH --mem=200GB
 #SBATCH --job-name=l-rr
@@ -17,25 +17,44 @@ conda activate tpot2-env-3.10
 
 # check if array task id is 1
 if [ $SLURM_ARRAY_TASK_ID -eq 1 ] ; then
-    DATA_DIR=/home/hernandezj45/Repos/lexidate-variation-analysis/Results/Lexicase/learn_20_select_80/
-    SPLIT_SELECT=0.80
+    DATA_DIR=/home/hernandezj45/Repos/lexidate-variation-analysis/Results/Lexicase/learn_05_select_95/
+    SPLIT_SELECT=0.95
     SCHEME=lexicase
-    SPLIT_OFFSET=500
-    EXP_OFFSET=0
+    SPLIT_OFFSET=0
+    EXP_OFFSET=10000
+    S=481
+    SEED=$((S + SPLIT_OFFSET + EXP_OFFSET))
+    TASK_ID=359954
+
+elif [ $SLURM_ARRAY_TASK_ID -eq 2 ] ; then
+    DATA_DIR=/home/hernandezj45/Repos/lexidate-variation-analysis/Results/Lexicase/learn_05_select_95/
+    SPLIT_SELECT=0.95
+    SCHEME=lexicase
+    SPLIT_OFFSET=0
+    EXP_OFFSET=10000
+    S=482
+    SEED=$((S + SPLIT_OFFSET + EXP_OFFSET))
+    TASK_ID=359955
+
+elif [ $SLURM_ARRAY_TASK_ID -eq 3 ] ; then
+    DATA_DIR=/home/hernandezj45/Repos/lexidate-variation-analysis/Results/Lexicase/learn_50_select_50/
+    SPLIT_SELECT=0.50
+    SCHEME=lexicase
+    SPLIT_OFFSET=1000
+    EXP_OFFSET=10000
     S=481
     SEED=$((S + SPLIT_OFFSET + EXP_OFFSET))
     TASK_ID=168757
 
-elif [ $SLURM_ARRAY_TASK_ID -eq 2 ] ; then
-    # Define the output directory
-    DATA_DIR=/home/hernandezj45/Repos/lexidate-variation-analysis/Results/Lexicase/learn_10_select_90/
-    SPLIT_SELECT=0.90
+elif [ $SLURM_ARRAY_TASK_ID -eq 4 ] ; then
+    DATA_DIR=/home/hernandezj45/Repos/lexidate-variation-analysis/Results/Lexicase/learn_95_select_05/
+    SPLIT_SELECT=0.05
     SCHEME=lexicase
-    SPLIT_OFFSET=0
-    EXP_OFFSET=0
+    SPLIT_OFFSET=2000
+    EXP_OFFSET=10000
     S=481
     SEED=$((S + SPLIT_OFFSET + EXP_OFFSET))
-    TASK_ID=359955
+    TASK_ID=168784
 fi
 
 # let it rip
@@ -43,6 +62,6 @@ python /home/hernandezj45/Repos/lexidate-variation-analysis/Source/experiment.py
 -split_select ${SPLIT_SELECT} \
 -scheme ${SCHEME} \
 -task_id ${TASK_ID} \
--n_jobs 12 \
+-n_jobs 10 \
 -savepath ${DATA_DIR} \
 -seed ${SEED} \
